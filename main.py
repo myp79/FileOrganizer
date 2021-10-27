bf9306ff67cbc4b612fa8a6b4d89589f3511f589
Mohammad YousefiPour <moyousefipour79@gmail.com>
2021-10-27 15:25:21 +0330

    make directory
    directories make and path save.

--- main.py
+++ main.py
@@ -10,5 +10,19 @@ def collect_extentions() -> set:
     for item in items:
         ext = os.path.splitext(item)[1]
         if ext != '':  # for files like .git
-            extentions.add(ext)
+            extentions.add(ext[1:])
     return extentions
+
+
+def make_directory(ext: set) -> set:
+    # make directories of extention and ready for move files
+    current_path = os.getcwd()
+    directory_path = set()
+    for item in ext:
+        try:  # if directory exist
+            path = os.path.join(current_path, item)
+            os.mkdir(path)
+            directory_path.add(path)
+        except OSError as error:
+            print(error)
+    return directory_path
